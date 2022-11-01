eaacde2aef5cfa75da0d57a38b77b5df7584c110
juharyimami <juharyimami2011@gmail.com>
2022-11-01 18:14:52 +0300

    route added with component

---
+++ src/components/Navebar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/calculator',
+      text: 'Calulator',
+    },
+    {
+      id: 3,
+      path: '/quote',
+      text: 'Quote',
+    },
+  ];
+
+  return (
+    <nav className="navBar">
+      <div className="logo">
+        <h1>Math Magicians</h1>
+      </div>
+
+      <ul className="menuNav">
+        {links.map((link) => (
+          <li key={link.id} className="navelink">
+            <NavLink
+              to={link.path}
+              activeClassName="active-link"
+              exact
+            >
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
