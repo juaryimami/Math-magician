22892a6e3f021f47652bc92c8fc035ee467c6268
bornittah <bornittah@gmail.com>
2022-12-07 11:30:06 +0300

    Add unit testing

---
+++ src/components/__tests__/Button.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import  Button from '../Button';
+
+it('renders button correctly', () => {
+  const tree = renderer
+    .create(<Button />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
